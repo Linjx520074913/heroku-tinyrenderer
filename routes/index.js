ee169e9de0b0883ee2d6924f20cdb5f3f0903910
linjingxiong <linjingxiong@orbbec.com>
2021-07-05 09:56:38 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({hello linjx})
+  res.json({message: 'hello linjx'})
 });
 
 module.exports = router;
